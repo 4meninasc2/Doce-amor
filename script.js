5aa6b6714f870f16c2b66371d29a34d6143cfce9
delicia&cia <133653349+4meninasc2@users.noreply.github.com>
2024-07-01 11:28:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Hilary Bittencout é uma linda moça de 22 anos, estava em busca de um emprego, mas não imaginaria ela que no meio desta busca encontraria o grande amor da sua vida. A história irá começar em:",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Um dia chuvoso",
+            "Um dia de sol"
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "Em uma manhã fria de New York Hilary continua em busca do seu novo emprego, camiinhando pelas ruas que estava presente as melhores e maiores empresas do país, a moça vizualizou a Jones Diamonsds que sempre foi seu sonhos de um dia trabalhar. Ao passar em frente dela acabou notando um anúcio de vaga de emprego  e logo se empolgou, apressou-se a entrar na empresa para entregar o curriculo. Ao entrar na empresa a recpcionista vai ser:", 
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Má",
+            "Gentil"
         ]
-    }
+    },
 ]; 
 
