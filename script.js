4ce6ab1b8050b80d86f90118193277b112b353d0
delicia&cia <133653349+4meninasc2@users.noreply.github.com>
2024-08-19 11:31:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,9 +15,12 @@ const perguntas = [
     {
         enunciado: "Em uma manhã fria de New York Hilary continua em busca do seu novo emprego, camiinhando pelas ruas que estava presente as melhores e maiores empresas do país, a moça vizualizou a Jones Diamonsds que sempre foi seu sonhos de um dia trabalhar. Ao passar em frente dela acabou notando um anúcio de vaga de emprego  e logo se empolgou, apressou-se a entrar na empresa para entregar o curriculo. Ao entrar na empresa a recpcionista vai ser:", 
         alternativas: [
-            "Má",
+            "Má"
             "Gentil"
         ]
     },
+    {
+        enunciado: "A recepisionista notou que HIlary tinha uma capacidade enorme para entrar lá, mas ficou com inveja por ela ser tão linda e disse (infelismente vocẽ não)"
+    }
 ]; 
 
